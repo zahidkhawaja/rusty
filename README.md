13c354944372dde8966996fd31d04aba64fafa5c
Zahid Khawaja <zahidkhawaja@icloud.com>
2022-10-04 18:04:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Rusty: GPT-3 Powered CLI Tool
+
+Convert natural language into executable commands directly from the terminal!
+
+Open source CLI tool powered by [OpenAI](https://openai.com/) (bring your own key).
+
+Feeling rusty on your bash skills? This might help! Built using Rust 🦀
+
+https://user-images.githubusercontent.com/21132214/193903587-1f6a51fe-b0d7-4f24-b95b-4bbd0e718ef2.mov
+
+## Getting Started
+
+1. Clone this repo.
+
+2. Create an environment variable `OPENAI_KEY` with your API key from [OpenAI](https://openai.com/api/).
+
+3. Build the Rust program.
+
+```bash
+cargo build --release
+```
+
+4. Add the binary to your path. Under target/release.
+
+## 🦀 Rust Docs
+
+- [Learn Rust](https://www.rust-lang.org/learn) - learn about the Rust programming language.
+
+## 🧠 OpenAI Docs
+
+- [OpenAI API](https://beta.openai.com/docs/introduction) from the creators of GPT-3.
+
+## Ping [Zahid](https://twitter.com/chillzaza_) if you need anything 🤘
+
+Stay tuned for updates.
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use hyper::{body::Buf, header, Body, Client, Request};
+use hyper_tls::HttpsConnector;
+use serde_derive::{Deserialize, Serialize};
+use std::{env, env::args};
+
+#[derive(Deserialize, Debug)]
+struct OpenAIChoices {
+    text: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct OpenAIResponse {
+    choices: Vec<OpenAIChoices>,
+}
+
+#[derive(Serialize, Debug)]
+struct OpenAIRequest {
+    model: String,
+    prompt: String,
+    max_tokens: u32,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Check for environment variable OPENAI_KEY
+    match env::var("OPENAI_KEY") {
+        Ok(_) => (),
+        Err(_) => {
+            println!("Error: please create an environment variable OPENAI_KEY");
+            std::process::exit(1);
+        }
+    }
+
+    let api_key = env::var("OPENAI_KEY").unwrap();
+
+    let https = HttpsConnector::new();
+    let client = Client::builder().build(https);
+    let uri = "https://api.openai.com/v1/completions";
+
+    let model = String::from("text-davinci-002");
+
+    let default_prompt =
+        "Given text, return 1 bash command. Text:list contents of a directory. Command:ls";
+    let mut user_input = String::new();
+
+    let mut arguments: Vec<String> = args().collect();
+    arguments.remove(0);
+
+    if arguments.len() == 0 {
+        println!("Welcome to Rusty! Enter an argument to get started.");
+        std::process::exit(1);
+    }
+
+    for x in arguments {
+        user_input.push_str(" ");
+        user_input.push_str(&x);
+    }
+
+    let auth_header_val = format!("Bearer {}", api_key);
+
+    let openai_request = OpenAIRequest {
+        model: model,
+        prompt: format!("{}. Text:{}. Command:", default_prompt, user_input),
+        max_tokens: 64,
+    };
+
+    let body = Body::from(serde_json::to_vec(&openai_request)?);
+
+    let req = Request::post(uri)
+        .header(header::CONTENT_TYPE, "application/json")
+        .header("Authorization", &auth_header_val)
+        .body(body)
+        .unwrap();
+
+    let res = client.request(req).await?;
+
+    let body = hyper::body::aggregate(res).await?;
+
+    let json: OpenAIResponse = serde_json::from_reader(body.reader())?;
+
+    println!("{}", json.choices[0].text);
+
+    Ok(())
+}
